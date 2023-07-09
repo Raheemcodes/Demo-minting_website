907a4f0ddb89d0789f588cca17529b87572cc232
Raheemcodes <raheemscorporatemail@gmail.com>
2023-07-10 00:10:53 +0100

    configure connect wallet feature

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -1,18 +1,28 @@
 import { Inject, Injectable } from '@angular/core';
-import Web3 from 'web3';
+import Web3, { Web3BaseProvider } from 'web3';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SharedService {
+  accounts!: string[];
   constructor(
     @Inject('Web3') private web3: Web3,
     @Inject('Window') private window: Window
   ) {}
 
-  connectWallet() {
+  async connectWallet() {
+    this.web3.givenProvider;
     if ('ethereum' in this.window) {
-      console.log(this.web3);
+      const response = await (this.window.ethereum as Web3BaseProvider).request(
+        {
+          method: 'eth_requestAccounts',
+        }
+      );
+
+      this.accounts = response as unknown as string[];
+
+      console.log(this.accounts);
     } else {
       // tell user to install metamask
     }
