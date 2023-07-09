41be058393d14adfde8637015500068eda0baeb6
Raheemcodes <raheemscorporatemail@gmail.com>
2023-07-09 08:44:49 +0100

    delete blur directive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -24,7 +24,6 @@ export class HeaderComponent implements OnInit {
 
   constructor(
     private renderer: Renderer2,
-
     @Inject(DOCUMENT) private document: Document
   ) {}
 
--- src/app/shared/load-blur.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoadBlurDirective } from './load-blur.directive';
-
-describe('LoadBlurDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoadBlurDirective();
-    expect(directive).toBeTruthy();
-  });
-});
