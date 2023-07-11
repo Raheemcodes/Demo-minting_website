c22fbba5f0c572322c9dab851bd75f0d73272ef6
Raheemcodes <raheemscorporatemail@gmail.com>
2023-07-12 00:40:11 +0100

    integrate totalSupply in template

--- src/app/mint/mint.component.ts
+++ src/app/mint/mint.component.ts
@@ -4,6 +4,7 @@ import Web3 from 'web3';
 import { SharedService } from '../shared/shared.service';
 import AzukiTransAbi from './AzukiTransAbi';
 import { Mint } from './mint.model';
+import { interval, min } from 'rxjs';
 
 @Component({
   selector: 'app-mint',
@@ -19,6 +20,8 @@ export class MintComponent implements OnInit {
   startTime!: string;
   publicsaleTime!: string;
   endTime!: string;
+  mintedSupply!: number;
+  totalSupply!: number;
 
   constructor(
     private sharedService: SharedService,
@@ -55,7 +58,55 @@ export class MintComponent implements OnInit {
     });
   }
 
+  formatTime(time: bigint): string {
+    const duration: number = Number(time) - Date.now() / 1000;
+    const oneMin = 60;
+    const oneHour = 60 * oneMin;
+    const oneDay = 24 * oneHour;
+
+    const days = Math.floor(Math.abs(duration) / oneDay);
+    const hours = Math.floor((Math.abs(duration) % oneDay) / oneHour);
+    const mins = Math.floor(((Math.abs(duration) % oneDay) % oneHour) / oneMin);
+
+    const remainingDays = !days ? '' : `${days} ${days > 1 ? 'days' : 'day'}`;
+    const remainingHours = !hours
+      ? ''
+      : `${hours} ${hours > 1 ? 'hours' : 'hour'}`;
+    const remainingMins = !mins ? '' : `${mins} ${mins > 1 ? 'mins' : 'min'}`;
+    const remainingTime: string = !duration
+      ? 'Now!!!'
+      : `${
+          duration > 0 ? 'In' : ''
+        } ${remainingDays} ${remainingHours} ${remainingMins} ${
+          duration < 0 ? 'ago' : ''
+        }`;
+
+    return remainingTime;
+  }
+
+  setTime() {
+    this.startTime = this.formatTime(this.mint.time.start);
+    this.publicsaleTime = this.formatTime(this.mint.time.publicSale);
+    this.endTime = this.formatTime(this.mint.time.end);
+  }
+
+  updateTime() {
+    this.setTime();
+    let period: number = 60 * 1000 - new Date().getSeconds();
+
+    interval(period).subscribe(() => {
+      period = 60 * 1000;
+
+      this.startTime = this.formatTime(this.mint.time.start);
+      this.publicsaleTime = this.formatTime(this.mint.time.publicSale);
+      this.endTime = this.formatTime(this.mint.time.end);
+    });
+  }
+
   async getMintDetails() {
     this.mint = await this.contract.methods.mint().call();
+    this.totalSupply = await this.contract.methods.totalSupply().call();
+    this.mintedSupply = Number(this.mint.total);
+    this.updateTime();
   }
 }
