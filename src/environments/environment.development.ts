fe4e1c26c84e746303d9dd076f2e6f00c64b7ea9
Raheemcodes <raheemscorporatemail@gmail.com>
2023-08-08 14:54:52 +0100

    Debug profile style compoennet

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   address: '0x004367AD8dF692d2ca4B413c9523523a7D391371',
   marketplace: '0x159ca70bbB1a4620a0EeA7289013Ab29ef4B80Bc',
-  API: 'http://localhost:5000',
+  API: 'https://z41qu1s0hl.execute-api.us-east-1.amazonaws.com/demo-dapp',
   baseURI: 'https://api.coolcatsnft.com/cat',
-  provider: 'ws://127.0.0.1:8545',
+  provider: 'wss://sepolia.infura.io/ws/v3/156bdc0c924f45c1aa7e9f1caf4cad09',
 };
