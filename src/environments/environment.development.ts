628ca08b5c22049bd45e8d4cf3bb6e95ae003067
Raheemcodes <raheemscorporatemail@gmail.com>
2023-08-09 14:52:59 +0100

    Change address

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  address: '0x004367AD8dF692d2ca4B413c9523523a7D391371',
-  marketplace: '0x159ca70bbB1a4620a0EeA7289013Ab29ef4B80Bc',
+  address: '0x29625e59A22Cba2Ba2c1c1F22Dc38774a8FB3E8D',
+  marketplace: '0x09Fd614a867AC8F56b1EBDD816D9100822E255a1',
   API: 'https://z41qu1s0hl.execute-api.us-east-1.amazonaws.com/demo-dapp',
   baseURI: 'https://api.coolcatsnft.com/cat',
   provider: 'wss://sepolia.infura.io/ws/v3/156bdc0c924f45c1aa7e9f1caf4cad09',
