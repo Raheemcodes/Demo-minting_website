a0e2400f8306daa2617fe1e23a6aac214d5709a4
Raheemcodes <raheemscorporatemail@gmail.com>
2024-06-13 17:25:52 +0100

    Change API

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   address: '0x27ECF50A9F9CbAFB6557f46E8A54dB3B670933DF',
   marketplace: '0xA5463d3E13fDdA02a876F41F144C6E2C041D8C25',
-  API: 'https://z41qu1s0hl.execute-api.us-east-1.amazonaws.com/demo-dapp',
+  API: 'https://a26n4cjqa6.execute-api.us-west-2.amazonaws.com/demo-server',
   baseURI: 'https://api.coolcatsnft.com/cat',
   provider: 'wss://sepolia.infura.io/ws/v3/156bdc0c924f45c1aa7e9f1caf4cad09',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   address: '0x9693c979fEd16EF96aA7426A41d6008152fb4f9c',
   marketplace: '0xcfb44B7bBAb2A1934ED6b8DD7a89f591428885FC',
-  API: 'https://z41qu1s0hl.execute-api.us-east-1.amazonaws.com/demo-dapp',
+  API: 'https://a26n4cjqa6.execute-api.us-west-2.amazonaws.com/demo-server',
   baseURI: 'https://api.coolcatsnft.com/cat',
   provider: 'wss://sepolia.infura.io/ws/v3/156bdc0c924f45c1aa7e9f1caf4cad09',
 };
