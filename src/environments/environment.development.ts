3a8f7291704bc052cc3a7932f583cd23cc8b4c25
Raheemcodes <raheemscorporatemail@gmail.com>
2024-06-14 22:05:51 +0100

    change addresses

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  address: '0x27ECF50A9F9CbAFB6557f46E8A54dB3B670933DF',
+  address: '0x9693c979fEd16EF96aA7426A41d6008152fb4f9c',
   marketplace: '0xA5463d3E13fDdA02a876F41F144C6E2C041D8C25',
   API: 'https://a26n4cjqa6.execute-api.us-west-2.amazonaws.com/demo-server',
   baseURI: 'https://api.coolcatsnft.com/cat',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   address: '0x9693c979fEd16EF96aA7426A41d6008152fb4f9c',
-  marketplace: '0xcfb44B7bBAb2A1934ED6b8DD7a89f591428885FC',
+  marketplace: '0xA5463d3E13fDdA02a876F41F144C6E2C041D8C25',
   API: 'https://a26n4cjqa6.execute-api.us-west-2.amazonaws.com/demo-server',
   baseURI: 'https://api.coolcatsnft.com/cat',
   provider: 'wss://sepolia.infura.io/ws/v3/156bdc0c924f45c1aa7e9f1caf4cad09',
